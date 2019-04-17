b8651308b3a9c43ba032a5bd44a5ceb6cf2b7876
Bruce <gkm85663@gmail.com>
2019-04-17 23:26:22 +0800

    import 'data table' at Index page

--- MemberMgmtSystem/App_Start/BundleConfig.cs
+++ MemberMgmtSystem/App_Start/BundleConfig.cs
@@ -16,6 +16,9 @@ namespace MemberMgmtSystem
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/dataTable").Include(
+                        "~/Scripts/Table/jquery.dataTables.min.js"));
+
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,7 +29,8 @@ namespace MemberMgmtSystem
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery.dataTables.min.css"));
         }
     }
 }
