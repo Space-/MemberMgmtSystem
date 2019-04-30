1acecacac89fb610d3d053f77edd939d350024e0
Bruce <gkm85663@gmail.com>
2019-05-01 00:31:22 +0800

    Remove unused comments

--- MemberMgmtSystem/Controllers/CustomersController.cs
+++ MemberMgmtSystem/Controllers/CustomersController.cs
@@ -10,13 +10,11 @@ namespace MemberMgmtSystem.Controllers
     {
         private Northwind db = new Northwind();
 
-        // GET: Customers
         public ActionResult Index()
         {
             return View(db.Customers.OrderByDescending(c => c.CustomerID).ToList());
         }
 
-        // GET: Customers/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -31,15 +29,11 @@ namespace MemberMgmtSystem.Controllers
             return View(customer);
         }
 
-        // GET: Customers/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Customers/Create
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "CustomerID,ContactName,Address,Phone,Age")] Customers customers)
@@ -54,27 +48,7 @@ namespace MemberMgmtSystem.Controllers
             return View(customers);
         }
 
-        // GET: Customers/Edit/5
-        //        public ActionResult Edit(int? id)
-        //        {
-        //            if (id == null)
-        //            {
-        //                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //            }
-        //            Customers customer = db.Customers.Find(id);
-        //            if (customer == null)
-        //            {
-        //                return HttpNotFound();
-        //            }
-        //            return View(customer);
-        //        }
-
-        // POST: Customers/Edit/5
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-        //        [HttpPost]
         [HttpGet, ActionName("Edit")]
-        //        [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "CustomerID,ContactName,Address,Phone,Age")] Customers customer)
         {
             if (ModelState.IsValid)
@@ -82,13 +56,10 @@ namespace MemberMgmtSystem.Controllers
                 db.Entry(customer).State = EntityState.Modified;
                 db.SaveChanges();
                 return Content("RefreshPage");
-                //                return Json(new { _view = RedirectToAction("Index"), validateResult = "fieldCorrect" });
             }
             return PartialView("_EditCustomer", customer);
-            //            return Json(new { _view = PartialView("_EditCustomer", customer), validateResult = "someFieldIncorrect" });
         }
 
-        // GET: Customers/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -101,12 +72,9 @@ namespace MemberMgmtSystem.Controllers
                 return HttpNotFound();
             }
 
-            //            return View(customer);
-            //            return PartialView("_DeleteCustomer", customer);
             return View("Delete", customer);
         }
 
-        // POST: Customers/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -126,7 +94,6 @@ namespace MemberMgmtSystem.Controllers
             base.Dispose(disposing);
         }
 
-        //        [HttpPost]
         public PartialViewResult PartialViewDelCustomer(int id)
         {
             var customer = db.Customers.Find(id);
